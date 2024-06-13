07030fa9f184a9bef4cff453ef2fce9d80297bd9
To Nguyen <32400888+binhnguyen1998822@users.noreply.github.com>
2024-06-13 10:48:46 +0700

    fix permalink

--- includes/Episode.php
+++ includes/Episode.php
@@ -43,12 +43,37 @@ function episodeUrl($current_url){
 
 function op_get_regions($end ='')
 {
+    $getslug = get_option('ophim_slug_regions');
+    if($getslug){
+        $slug = $getslug;
+    }else{
+        $slug = 'regions';
+    }
     global $post;
     $html = "";
     $country = get_the_terms($post->ID, "ophim_regions");
     if (is_array($country)) {
         foreach ($country as $ct) {
-            $html .= "<a href=\"" . home_url(get_option('ophim_slug_regions') ? get_option('ophim_slug_regions') : 'regions' . "/" . $ct->slug) . "\" title=\"" . $ct->name . "\">" . $ct->name . "</a>". $end;
+            $html .= "<a href=\"" . home_url($slug . "/" . $ct->slug) . "\" title=\"" . $ct->name . "\">" . $ct->name . "</a>". $end;
+        }
+    }
+    return $html;
+}
+
+function op_get_years($end ='')
+{
+    $getslug = get_option('ophim_slug_years');
+    if($getslug){
+        $slug = $getslug;
+    }else{
+        $slug = 'years';
+    }
+    global $post;
+    $html = "";
+    $years = get_the_terms($post->ID, "ophim_years");
+    if (is_array($years)) {
+        foreach ($years as $y) {
+            $html .= "<a href=\"" . home_url($slug . "/" . $y->slug) . "\" title=\"" . $y->name . "\">" . $y->name . "</a>". $end;
         }
     }
     return $html;
@@ -56,12 +81,18 @@ function op_get_regions($end ='')
 
 function op_get_genres($end ='')
 {
+    $getslug = get_option('ophim_slug_genres');
+    if($getslug){
+        $slug = $getslug;
+    }else{
+        $slug = 'genres';
+    }
     global $post;
     $html = "";
     $genres = get_the_terms($post->ID, "ophim_genres");
     if (is_array($genres)) {
         foreach ($genres as $genre) {
-            $html .= "<a href=\"" . home_url(get_option('ophim_slug_genres') ? get_option('ophim_slug_genres') : 'genres' . "/" . $genre->slug) . "\" title=\"" . $genre->name . "\">" . $genre->name . "</a>".$end;
+            $html .= "<a href=\"" . home_url($slug . "/" . $genre->slug) . "\" title=\"" . $genre->name . "\">" . $genre->name . "</a>".$end;
         }
     }
     return $html;
@@ -69,36 +100,54 @@ function op_get_genres($end ='')
 
 function op_get_tags($end ='')
 {
+    $getslug = get_option('ophim_slug_tags');
+    if($getslug){
+        $slug = $getslug;
+    }else{
+        $slug = 'tags';
+    }
     global $post;
     $html = "";
     $tags = get_the_terms($post->ID, "ophim_tags");
     if (is_array($tags)) {
         foreach ($tags as $tag) {
-            $html .= "<a href=\"" . home_url(get_option('ophim_slug_tags') ? get_option('ophim_slug_tags') : 'tags' . "/" . $tag->slug) . "\" title=\"" . $tag->name . "\">" . $tag->name . "</a>".$end;
+            $html .= "<a href=\"" . home_url($slug . "/" . $tag->slug) . "\" title=\"" . $tag->name . "\">" . $tag->name . "</a>".$end;
         }
     }
     return $html;
 }
 function op_get_actors($limit = 10,$end ='')
 {
+    $getslug = get_option('ophim_slug_actors');
+    if($getslug){
+        $slug = $getslug;
+    }else{
+        $slug = 'actors';
+    }
     global $post;
     $html = "";
     $actors = get_the_terms($post->ID, "ophim_actors");
     if (is_array($actors)) {
         foreach (array_slice($actors, 0, $limit) as $actor) {
-            $html .= "<a href=\"" . home_url(get_option('ophim_slug_actors') ? get_option('ophim_slug_actors') : 'actors' . "/" . $actor->slug) . "\" title=\"" . $actor->name . "\">" . $actor->name . "</a>".$end;
+            $html .= "<a href=\"" . home_url($slug . "/" . $actor->slug) . "\" title=\"" . $actor->name . "\">" . $actor->name . "</a>".$end;
         }
     }
     return $html;
 }
 function op_get_directors($limit = 10,$end ='')
 {
+    $getslug = get_option('ophim_slug_directors');
+    if($getslug){
+        $slug = $getslug;
+    }else{
+        $slug = 'directors';
+    }
     global $post;
     $html = "";
     $directors = get_the_terms($post->ID, "ophim_directors");
     if (is_array($directors)) {
         foreach (array_slice($directors, 0, $limit) as $director) {
-            $html .= "<a href=\"" . home_url(get_option('ophim_directors') ? get_option('ophim_directors') : 'directors' . "/" . $director->slug) . "\" title=\"" . $director->name . "\">" . $director->name . "</a>".$end;
+            $html .= "<a href=\"" . home_url($slug . "/" . $director->slug) . "\" title=\"" . $director->name . "\">" . $director->name . "</a>".$end;
         }
     }
     return $html;
