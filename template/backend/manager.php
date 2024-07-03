0262d1edfdd126d5fbfbc8644b3a42e555084548
To Nguyen <32400888+binhnguyen1998822@users.noreply.github.com>
2024-07-03 19:28:35 +0700

    add guide

--- template/backend/manager.php
+++ template/backend/manager.php
@@ -1,2 +1,15 @@
 <h1>Xin chào</h1>
 <p>OPhim Core cho wordpress. CMS tạo website xem phim với nhiều giao diện đa dạng, dữ liệu phim miễn phí. Chỉ nên tải mã nguồn chính thức từ diễn đàn <a href="https://forum.ophim.cc/"> https://forum.ophim.cc/</a></p>
+<hr>
+<h2>Hướng dẫn sử dụng: </h2>
+<h3>Lỗi 404</h3>
+<p>Vào Cài đặt -> <a href="/wp-admin/options-permalink.php"> Đường dẫn tĩnh</a> -> Lưu</p>
+<h3>Thêm Menu</h3>
+<p>Vào Giao diện -> <a href="/wp-admin/nav-menus.php"> Menu</a> -> Tạo Primary Menu<p>
+    <p>Trên cùng bên phải -> Tùy chọn hiển thị -> Tích chọn danh mục của oPhim</p>
+<h3>Thêm widget</h3>
+<p>Vào Giao diện -> <a href="/wp-admin/widgets.php"> Widget</a> -> Kéo widget vào phần muốn hiển thị<p>
+<h3>Sửa logo</h3>
+<p>Vào Giao diện -> <a href="/wp-admin/customize.php"> Tùy biến</a> -> Nhận dạng site<p>
+<h3>Số bài hiển thị trong trang</h3>
+<p>Vào Cài đặt -> <a href="/wp-admin/options-reading.php"> Đọc</a> -> Hiển thị nhiều nhất<p>
